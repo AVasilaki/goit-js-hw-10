27cfb156a06580b51a1c844abdb0c58b91c8711f
AVasilaki <poseydon463@gmail.com>
2023-08-25 11:28:03 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_0JtGVSBqjiasWh5nfxpRM1Q6z3bcX9aPfbx9j9i4imJDYV6mB8JMAUyzAvBAq9oV';
+const select = document.querySelector('select');
+axios
+  .get('https://api.thecatapi.com/v1/breeds')
+  .then(function (response) {
+    console.log(response.data);
+    select.insertAdjacentHTML('beforeend', createMarckup(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {});
+
+function createMarckup(arr) {
+  return arr
+    .map(({ id, name }) => `<option value="${id}">${name}</option> `)
+    .join('');
+}
