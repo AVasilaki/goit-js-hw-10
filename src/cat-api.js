ca3a93af7778778892420d2535d1583491dd56b1
AVasilaki <poseydon463@gmail.com>
2023-08-26 11:17:14 +0300

    dz update

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,19 @@
+import axios from 'axios';
+import { createMarckup, select } from '.';
+axios.defaults.headers.common['x-api-key'] =
+  'live_0JtGVSBqjiasWh5nfxpRM1Q6z3bcX9aPfbx9j9i4imJDYV6mB8JMAUyzAvBAq9oV';
+// const select = document.querySelector('select');
 function fetchBreeds() {
-  return fetch(
-    'https://api.thecatapi.com/v1/breeds?api_key=live_0JtGVSBqjiasWh5nfxpRM1Q6z3bcX9aPfbx9j9i4imJDYV6mB8JMAUyzAvBAq9oV'
-  );
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(function (response) {
+      console.log(response);
+      select.insertAdjacentHTML('beforeend', createMarckup(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(function () {});
 }
 
 export { fetchBreeds };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import axios from 'axios';
 import { fetchBreeds } from './cat-api';
-fetchBreeds();
-axios.defaults.headers.common['x-api-key'] =
-  'live_0JtGVSBqjiasWh5nfxpRM1Q6z3bcX9aPfbx9j9i4imJDYV6mB8JMAUyzAvBAq9oV';
-const select = document.querySelector('select');
-axios
-  .get('https://api.thecatapi.com/v1/breeds')
-  .then(function (response) {
-    console.log(response.data);
-    select.insertAdjacentHTML('beforeend', createMarckup(response.data));
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .finally(function () {});
 
+// axios.defaults.headers.common['x-api-key'] =
+//   'live_0JtGVSBqjiasWh5nfxpRM1Q6z3bcX9aPfbx9j9i4imJDYV6mB8JMAUyzAvBAq9oV';
+const select = document.querySelector('select');
+// axios
+//   .get('https://api.thecatapi.com/v1/breeds')
+//   .then(function (response) {
+//     console.log(response.data);
+//     select.insertAdjacentHTML('beforeend', createMarckup(response.data));
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .finally(function () {});
+fetchBreeds();
 function createMarckup(arr) {
   return arr
     .map(({ id, name }) => `<option value="${id}">${name}</option> `)
     .join('');
 }
+select.addEventListener('change', handlerCat);
+function handlerCat(evt) {
+  const cat = evt.currentTarget.value;
+  console.log(cat);
+}
+
+export { createMarckup, select };
