864be63ff73ef45174c0f9af079f4bd830b63873
AVasilaki <poseydon463@gmail.com>
2023-08-27 14:10:54 +0300

    upd
    вірні проміси

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,31 +1,29 @@
 import axios from 'axios';
-import { createMarckup, select, inform, catMarkup } from '.';
+
 axios.defaults.headers.common['x-api-key'] =
   'live_0JtGVSBqjiasWh5nfxpRM1Q6z3bcX9aPfbx9j9i4imJDYV6mB8JMAUyzAvBAq9oV';
-// const select = document.querySelector('select');
+
 function fetchBreeds() {
   return axios
     .get('https://api.thecatapi.com/v1/breeds')
     .then(function (response) {
-      console.log(response);
-      select.insertAdjacentHTML('beforeend', createMarckup(response.data));
+      return response.data;
     })
     .catch(function (error) {
-      console.log(error);
+      return error;
     })
     .finally(function () {});
 }
 
 function fetchCatByBreed(breedId) {
-  console.log(breedId, 'breedId');
   return axios
     .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
     .then(function (response) {
-      console.log(response.data[0], 'a');
-      inform.insertAdjacentHTML('beforeend', catMarkup(response.data));
+      return response.data;
     })
+
     .catch(function (error) {
-      console.log(error);
+      return error;
     })
     .finally(function () {});
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import { fetchBreeds, fetchCatByBreed } from './cat-api';
 const select = document.querySelector('select');
 const inform = document.querySelector('div');
 
-fetchBreeds();
+fetchBreeds()
+  .then(data => select.insertAdjacentHTML('beforeend', createMarckup(data)))
+  .catch();
+
 function createMarckup(arr) {
   return arr
     .map(({ id, name }) => `<option value="${id}">${name}</option> `)
@@ -12,9 +15,10 @@ function createMarckup(arr) {
 select.addEventListener('change', handlerCat);
 function handlerCat(evt) {
   const cat = evt.currentTarget.value;
-  console.log(cat);
-  fetchCatByBreed(cat);
-  // inform.innerHTML = '';
+  inform.innerHTML = '';
+  fetchCatByBreed(cat)
+    .then(data => inform.insertAdjacentHTML('beforeend', catMarkup(data)))
+    .catch();
 }
 
 function catMarkup(arr) {
@@ -32,4 +36,3 @@ function catMarkup(arr) {
     )
     .join('');
 }
-export { createMarckup, select, inform, catMarkup };
